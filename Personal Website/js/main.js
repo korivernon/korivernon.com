d6349eba57ba944a0e04b3db2249356d15f31372
korivernon <44047473+korivernon@users.noreply.github.com>
2019-05-29 11:08:40 -0400

    personal website

---
+++ Personal Website/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var typed = new Typed('.typed',{
+    strings: ['Kori Vernon.', '<em>a programmer.</em>',
+    '<em>a photographer.</em>', '<em> a videographer.' , 'Kori Vernon.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+  });
+});
