f0987b6cae83ea3b438df7d841d56a42cf933d55
korivernon <44047473+korivernon@users.noreply.github.com>
2019-06-13 11:56:37 -0400

    adding jokes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var typed = new Typed('.typed',{
     strings: ['Kori Vernon.', '<em>a programmer.</em>',
-    '<em>a photographer.</em>', '<em> a videographer.' , 'Kori Vernon.'],
+    '<em>a photographer.</em>', '<em> a videographer.' , 'not done yet, chill.'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
