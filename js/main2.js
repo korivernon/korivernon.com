0c0f69ec39440a0ca553c2285d6e33e9c73c872e
korivernon <44047473+korivernon@users.noreply.github.com>
2019-09-01 19:36:11 -0400

    latest

--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   var typed = new Typed('.typed',{
-    strings: ['Kori Vernon.', '<em>a programmer.</em>',
-    '<em>a photographer.</em>', '<em> a videographer.'],
+    strings: ['So you want to be a Computer Scientist!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
