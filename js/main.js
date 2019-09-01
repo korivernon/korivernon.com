7fecd537d922235f2177f7c8633cd07f03613d88
korivernon <44047473+korivernon@users.noreply.github.com>
2019-09-01 19:37:48 -0400

    latest

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   var typed = new Typed('.typed',{
-    strings: ['Kori Vernon.', '<em>a programmer.</em>',
-    '<em>a photographer.</em>', '<em> a videographer.'],
+    strings: ['<em>a programmer.</em>', 'Kori Vernon.'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
