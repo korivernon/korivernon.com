874a8f7f286991dab2ff1379bc3d2c891b6d5be4
korivernon <44047473+korivernon@users.noreply.github.com>
2020-03-13 14:33:53 +0400

    latest

--- js/main3.js
+++ js/main3.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var typed = new Typed('.typed',{
-    strings: ['chegg ;)'],
+    strings: ['so ya need chegg ;)'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
